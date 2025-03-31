0b218ae9ec66d02f6fbeb78351ca98d5570511b2
Matheus Rezende <112785388+tiaszz@users.noreply.github.com>
2025-03-31 14:35:47 +0000

    adding flags

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	args := os.Args
-	fmt.Println(args)
+	numberBytes := flag.Bool("c", false, "Get the number of bytes of a file")
+	numberLines := flag.Bool("l", false, "Get the number of lines of a file")
+	numberWords := flag.Bool("w", false, "Get the number of words of a file")
+
+	flag.Parse()
+	fmt.Printf("%v\n%v\n%v\n", *numberBytes, *numberLines, *numberWords)
 }
